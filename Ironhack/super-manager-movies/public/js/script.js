5c85ce15bc68c6108d1d8995be68a45af2d4b57a
“Marcela <mpmontiela@gmail.com>
2022-11-03 13:11:29 +0100

    hello

---
+++ Ironhack/super-manager-movies/public/js/script.js
@@ -0,0 +1,4 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("super-manager-project JS imported successfully!");
+});
