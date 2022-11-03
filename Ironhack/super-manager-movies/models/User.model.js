f69eb9ddc142096b64af6d7b07c539410545a9f5
“Marcela <mpmontiela@gmail.com>
2022-11-03 15:11:57 +0100

    starting2

--- Ironhack/super-manager-movies/models/User.model.js
+++ Ironhack/super-manager-movies/models/User.model.js
@@ -9,13 +9,7 @@ const userSchema = new Schema(
       unique: true,
       trim: true,
     },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-      lowercase: true,
-    },
+    
     password: {
       type: String,
       required: true,
