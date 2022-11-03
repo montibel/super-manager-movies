48e338089c73805eb9608a0d11f9e05f9f124d77
“Marcela <mpmontiela@gmail.com>
2022-11-03 12:09:34 +0100

    starting

--- Ironhack/super-manager-project/config/index.js
+++ Ironhack/super-manager-project/config/index.js
@@ -1,38 +1,66 @@
 // We reuse this import in order to have access to the `body` property in requests
-const express = require("express");
+const express = require('express')
+
+// Handles ejs
+const expressLayouts = require('express-ejs-layouts')
 
 // ℹ️ Responsible for the messages you see in the terminal as requests are coming in
 // https://www.npmjs.com/package/morgan
-const logger = require("morgan");
+const logger = require('morgan')
 
 // ℹ️ Needed when we deal with cookies (we will when dealing with authentication)
 // https://www.npmjs.com/package/cookie-parser
-const cookieParser = require("cookie-parser");
+const cookieParser = require('cookie-parser')
 
-// ℹ️ Needed to accept requests from 'the outside'. CORS stands for cross origin resource sharing
-// unless the request is made from the same domain, by default express wont accept POST requests
-const cors = require("cors");
+// ℹ️ Serves a custom favicon on each request
+// https://www.npmjs.com/package/serve-favicon
+const favicon = require('serve-favicon')
 
-const FRONTEND_URL = process.env.ORIGIN || "http://localhost:3000";
+// ℹ️ global package used to `normalize` paths amongst different operating systems
+// https://www.npmjs.com/package/path
+const path = require('path')
+
+// ℹ️ Session middleware for authentication
+// https://www.npmjs.com/package/express-session
+const session = require('express-session')
+
+// ℹ️ MongoStore in order to save the user session in the database
+// https://www.npmjs.com/package/connect-mongo
+const MongoStore = require('connect-mongo')
+
+// Connects the mongo uri to maintain the same naming structure
+const MONGO_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/super-manager-project'
 
 // Middleware configuration
-module.exports = (app) => {
-  // Because this will be hosted on a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
-  // Services like Fly use something called a proxy and you need to add this to your server
-  app.set("trust proxy", 1);
-
-  // controls a very specific header to pass headers from the frontend
-  app.use(
-    cors({
-      origin: [FRONTEND_URL],
-    })
-  );
-
+module.exports = app => {
   // In development environment the app logs
-  app.use(logger("dev"));
+  app.use(logger('dev'))
 
   // To have access to `body` property in the request
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  app.use(cookieParser());
-};
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: false }))
+  app.use(cookieParser())
+
+  // Normalizes the path to the views folder
+  app.set('views', path.join(__dirname, '..', 'views'))
+  // Sets the view engine to ejs
+  app.set('view engine', 'ejs')
+  app.use(expressLayouts)
+  // Handles access to the public folder
+  app.use(express.static(path.join(__dirname, '..', 'public')))
+
+  // Handles access to the favicon
+  app.use(favicon(path.join(__dirname, '..', 'public', 'images', 'favicon.ico')))
+
+  // ℹ️ Middleware that adds a "req.session" information and later to check that you are who you say you are 😅
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET || 'super hyper secret key',
+      resave: false,
+      saveUninitialized: false,
+      store: MongoStore.create({
+        mongoUrl: MONGO_URI,
+      }),
+    })
+  )
+}
