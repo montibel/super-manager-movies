cd35c33d105be5658fbe453ab542bea87e0198a5
89Ih <ehabhamza94@gmail.com>
2022-11-05 10:42:13 +0100

    get all movies .... done

--- Ironhack/super-manager-movies/routes/movies.routes.js
+++ Ironhack/super-manager-movies/routes/movies.routes.js
@@ -1,25 +1,43 @@
 const express = require('express');
 const router = express.Router();
-
+const tmdb = require('tmdbv3').init("a5744544657b8f40d3829259594d6e5a");
 const axios = require("axios");
-var url = require('url');
+
 
 const { render } = require("ejs");
 
-/* GET movies  */
-// router.get('/list-movies', (req, res, next) => {
-  
 
-// });
-router.get('/movie', (req, res, next) => {
+
+router.get('/movie', (req, res) => {
   
     axios.get("https://api.themoviedb.org/3/movie/550?api_key=a5744544657b8f40d3829259594d6e5a")
     .then(responseFromAPI => {
-        console.log(req.path);
-        console.log(responseFromAPI.data);
+        // console.log(req.path);
+        // console.log(responseFromAPI.data);
         res.render("mov/movie", { movies: responseFromAPI.data});
     })
     .catch(err => console.error(err))
 });
+
+router.get('/lists', (req, res) => {
+    
+    axios.get("https://api.themoviedb.org/3/list/8168597?api_key=a5744544657b8f40d3829259594d6e5a&language=en-US") 
+    .then(listAPI => {
+            
+        res.render("mov/lists", {listAPI: listAPI.data.items});
+    })
+  .catch(err => console.error(err))   
+});
+
+router.get('/col', (req, res) => {
+    
+    axios.get("https://api.themoviedb.org/3/movie/550/lists?api_key=a5744544657b8f40d3829259594d6e5a&language=en-US") 
+    .then(a => {
+            console.log(a.data)
+        res.send("<h1>Hello</h1>");
+    })
+  .catch(err => console.error(err))   
+});
+
 module.exports = router;
 
